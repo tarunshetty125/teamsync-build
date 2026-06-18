2b1346e2f1c6fe0ae94f8b17621e4bc4aebd5e54
TarunShetty125 <tarunshetty125@gmail.com>
2026-06-19 00:16:50 +0530

    feat(config): add electron config constants

---
+++ electron/config/constants.ts
@@ -0,0 +1,5 @@
+// electron/config/constants.ts
+// Shared application constants.
+
+/** Sentinel key used to identify trial-license entries in settings/DB. */
+export const TRIAL_SENTINEL_KEY = '__trial__';
